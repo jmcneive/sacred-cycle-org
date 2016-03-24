fb9d5e2704898b2cb351702dea8e8be40b76331e
jmcneive <33woodland>
2016-03-24 08:34:17 -0500

    (Dev) Navigation

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,20 +9,23 @@
 
 		<a href="#"><img class="ssLogo__home--splash" src="/img/sacred-cycle-logo-wht-text-horz.svg" alt=""></a>
 
+		<h3 class="headline__home--splash">IF YOU'VE SURVIVED SEXUAL TRAUMA YOU KNOW THAT HEALING IS A CYCLE</h1>
+
 	</div>
 
-	<ul class="navigation">
-		<a href="#"><li class="navigation-link">Home</li></a>
-		<a href="#"><li class="navigation-link">About</li></a>
-		<a href="#"><li class="navigation-link">Donate</li></a>
-		<a href="#"><li class="navigation-link">Apply Here</li></a>
-		<a href="#"><li class="navigation-link">For Survivors</li></a>
-		<a href="#"><li class="navigation-link">Team Riders</li></a>
-	</ul>
 
 	<div id="mission" class="panel">
 
-		<a href="#"><img class="ssLogo__home--mission" src="/img/sacred-cycle-logo-grn-text-vert.svg" alt=""></a>
+		<a href="#"><img class="ssLogo__home--mission" src="/img/sacred-cycle-logo-grn-text-vert.png" alt=""></a>
+
+		<ul class="navigation">
+			<a href="#"><li class="navigation-link">Home</li></a>
+			<a href="#"><li class="navigation-link">About</li></a>
+			<a href="#"><li class="navigation-link">Donate</li></a>
+			<a href="#"><li class="navigation-link">Apply Here</li></a>
+			<a href="#"><li class="navigation-link">For Survivors</li></a>
+			<a href="#"><li class="navigation-link">Team Riders</li></a>
+		</ul>
 
 	</div>
 
